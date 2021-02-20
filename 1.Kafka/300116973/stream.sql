fbe1ff393f244ea9a5c888dce88a465ad6d6d2b7
unknown <lagokamdem87@gmail.com>
2021-02-20 11:05:58 -0500

    :horse::shark:

--- 1.Kafka/300116973/stream.sql
+++
@@ -1,9 +0,0 @@
-CREATE STREAM pageviews
-  (viewtime BIGINT,
-   userid VARCHAR,
-   pageid VARCHAR)
-  WITH (KAFKA_TOPIC='pageviews',
-        VALUE_FORMAT='DELIMITED',
-        PARTITIONS=2,
-        KEY='pageid',
-        TIMESTAMP='viewtime');
