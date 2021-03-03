ab45665458738cefd294a99d3424d743f1551316
Auriane25 <55238107+Auriane25@users.noreply.github.com>
2021-03-02 19:51:56 -0500

    Update stream.sql

--- 2.KSQL/300116670/stream.sql
+++ 2.KSQL/300116670/stream.sql
@@ -1,1 +1,21 @@
+CREATE STREAM Aurianeserver_withkey (
+    host string,
+    message string,
+    timestamp VARCHAR,
+    _MACHINE_ID VARCHAR,
+    _SYSTEMD_UNIT string
+    
+) WITH (
+    kafka_topic = 'topic-journald',
+    value_format = 'json',
+    timestamp = 'timestamp',                        
+    timestamp_format = 'yyyy-MM-dd''T''HH:mm:ss.nnnnnn''Z''',
+    KEY='_MACHINE_ID'
+); 
+ 
 
+
+
+
+
+                                                           
