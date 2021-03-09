59ebf11614360e76b87fa203086bbfcbfbf14a94
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2021-03-09 09:56:35 -0500

    :egg::bread:

--- 2.KSQL/300116973/stream.sql
+++ 2.KSQL/300116973/stream.sql
@@ -1,10 +1,14 @@
 CREATE STREAM nathy
-  (_CAP_EFFECTIVE VARCHAR,
-   _BOOT_ID INT,
+  (viewtime BIGINT,
+   _CAP_EFFECTIVE VARCHAR,
+   _BOOT_ID VARCHARD,
    __MONOTONIC_TIMESTAMP VARCHAR,
-   __REALTIME_TIMESTAMP VARCHAR)
+   __REALTIME_TIMESTAMP VARCHAR),
+   timestamp VARCHAR
   WITH (KAFKA_TOPIC='topic-journald',
         VALUE_FORMAT='json',
-        KEY='_BOOT_ID',                       
-    timestamp_format = 'yyyy-MM-dd''T''HH:mm:ss.nnnnnn''Z''' 
+        Partition=1
+        KEY='_BOOT_ID',
+        timestamp = 'timestamp',                        -- the column to use as a timestamp
+        timestamp_format = 'yyyy-MM-dd''T''HH:mm:ss.nnnnnn''Z''' -- the format to parse the timestamp
         );
