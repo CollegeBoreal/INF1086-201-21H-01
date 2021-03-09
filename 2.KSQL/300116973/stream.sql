4fab260c408a9efaa8fadfd9aaf0ed0b6014c61e
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2021-03-08 22:06:15 -0500

    :egg::bread::milk:

--- 2.KSQL/300116973/stream.sql
+++ 2.KSQL/300116973/stream.sql
@@ -5,7 +5,6 @@ CREATE STREAM nathy
    __REALTIME_TIMESTAMP VARCHAR)
   WITH (KAFKA_TOPIC='topic-journald',
         VALUE_FORMAT='json',
-        PARTITIONS=1,
         KEY='_BOOT_ID',                       
     timestamp_format = 'yyyy-MM-dd''T''HH:mm:ss.nnnnnn''Z''' 
         );
