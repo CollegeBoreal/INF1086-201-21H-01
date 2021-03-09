6141566276e3e85ccd7847854edbc5b3d1b39063
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2021-03-08 19:34:47 -0500

    :egg:

--- 2.KSQL/300116973/stream.sql
+++ 2.KSQL/300116973/stream.sql
@@ -1,12 +1,12 @@
 CREATE STREAM nathy
-  (rowkey STRING KEY,
-    _CAP_EFFECTIVE VARCHAR,
-   _STREAM_ID BIGINT,
-   __MONOTONIC_TIMESTAMP VARCHAR,
-   _SYSTEMD_INVOCATION_ID VARCHAR)
+  (viewtime BIGINT,
+   userid VARCHAR,
+   pageid VARCHAR,
+   timestamp VARCHAR)
   WITH (KAFKA_TOPIC='topic-journald',
         VALUE_FORMAT='json',
         PARTITIONS=1,
-        KEY='_STREAM_ID',                       
+        KEY='pageid',
+        timestamp = 'timestamp',                        
     timestamp_format = 'yyyy-MM-dd''T''HH:mm:ss.nnnnnn''Z''' 
         );
