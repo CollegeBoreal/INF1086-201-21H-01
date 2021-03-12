9321933c203aef4728737a50aa78332f5c5f619d
willfrid boris <boriskwayep35@yahoo.com>
2021-03-12 13:28:21 -0500

    :whale2:

--- 2.KSQL/stream.sql
+++
@@ -1,5 +0,0 @@
-CREATE STREAM logEvents(host string, PRIORITY int, timestamp string, message string);
-WITH (kafka_topic='topic-journald',value_format='json');
-CREATE STREAM MaxlogEvents
-AS SELECT * FROM logEvents
-WHERE PRIORITY>=6 ;
