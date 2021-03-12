a61cd2ca5216cd5fc363b754a9c247cd4675045b
lagokamdem <54910252+lagokamdem@users.noreply.github.com>
2021-03-12 13:44:33 -0500

    :candy::car:

--- 2.KSQL/300116973/stream.sql
+++ 2.KSQL/300116973/stream.sql
@@ -1,14 +1,11 @@
-CREATE STREAM nathy
-  (viewtime BIGINT,
-   _CAP_EFFECTIVE VARCHAR,
-   _BOOT_ID VARCHARD,
-   __MONOTONIC_TIMESTAMP VARCHAR,
-   __REALTIME_TIMESTAMP VARCHAR),
-   timestamp VARCHAR
-  WITH (KAFKA_TOPIC='topic-journald',
-        VALUE_FORMAT='json',
-        Partition=1
-        KEY='_BOOT_ID',
-        timestamp = 'timestamp',                        -- the column to use as a timestamp
-        timestamp_format = 'yyyy-MM-dd''T''HH:mm:ss.nnnnnn''Z''' -- the format to parse the timestamp
-        );
+CREATE STREAM NATHY (
+    __REALTIME_TIMESTAMP string,
+    message string,
+    _SOURCE_REALTIME_TIMESTAMP VARCHAR,
+    _MACHINE_ID VARCHAR,
+    _SYSTEMD_UNIT string
+    
+) WITH (
+    kafka_topic = 'topic-journald',
+    value_format = 'json'   
+); 
