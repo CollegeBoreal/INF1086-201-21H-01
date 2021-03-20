6efa169bb5576a6de84bd5567083eec28301e912
Idir <54910261+dido05327@users.noreply.github.com>
2021-03-20 16:47:49 -0400

    Update stream.sql

--- 2.KSQL/300117444/stream.sql
+++ 2.KSQL/300117444/stream.sql
@@ -1,7 +1,7 @@
 
   
-CREATE STREAM logEvents(host string, PRIORITY int, timestamp string, message string)
+CREATE STREAM idirStream(host string, PRIORITY int, timestamp string, message string)
 WITH (kafka_topic='topic-journald',value_format='json');
-CREATE STREAM MaxlogEvents
-AS SELECT * FROM logEvents
-WHERE PRIORITY>=6 ;
+CREATE STREAM IdirStream2
+AS SELECT * FROM idirStream
+WHERE PRIORITY>=4 ;
