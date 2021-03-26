f605169630e78b56f4af62d36d8f3190d24be00d
Zack Mouloud Sadi-Bourouih <zackstoronto@gmail.com>
2021-03-25 21:25:15 -0400

    Update stream.sql

--- 2.KSQL/300115140/stream.sql
+++ 2.KSQL/300115140/stream.sql
@@ -8,3 +8,23 @@ CREATE STREAM systemd (
      timestamp = 'timestamp',                        -- the column to use as a timestamp
      timestamp_format = 'yyyy-MM-dd''T''HH:mm:ss.nnnnnn''Z''' -- the format to parse the timestamp
 );
+
+
+CREATE STREAM s_pageviews \
+  (userid VARCHAR, \
+   registertime BIGINT, \
+   pageid VARCHAR) \
+  WITH (KAFKA_TOPIC='pageviews_kafka_topic_json', \
+        VALUE_FORMAT='JSON', \
+        TIMESTAMP='registertime', \
+        KEY = 'userid');
+        
+        
+CREATE STREAM s_users \
+  (userid VARCHAR, \
+   registertime BIGINT, \
+   regionid VARCHAR) \
+  WITH (KAFKA_TOPIC='users_kafka_topic_json', \
+        VALUE_FORMAT='JSON', \
+        TIMESTAMP='registertime', \
+        KEY = 'userid');        
