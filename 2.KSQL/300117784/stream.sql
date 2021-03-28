07a4535aeb4a040546b4096cc6b52ae4448611b5
BertrandMoyou <54910102+BertrandMoyou@users.noreply.github.com>
2021-03-28 17:36:42 -0400

    Update stream.sql

--- 2.KSQL/300117784/stream.sql
+++ 2.KSQL/300117784/stream.sql
@@ -1,5 +1,5 @@
- CREATE STREAM Logservices(PRIORITY string, SYSLOG_IDENTIFIER string , host string , message string)
+CREATE STREAM Logservices(PRIORITY string, SYSLOG_IDENTIFIER string , host string , message string)
  WITH (kafka_topic = 'topic-journald', value_format = 'json');
 
-	CREATE STREAM EventsIdentifier
+CREATE STREAM EventsIdentifier
 AS SELECT SYSLOG_IDENTIFIER, message  FROM Logservices;
