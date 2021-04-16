d6d843909be7f62adb6190df2b33741af842ea3d
djibo648 <54910103+djibo648@users.noreply.github.com>
2021-04-15 22:37:06 -0400

    Update table.sql

--- 2.KSQL/300117806/table.sql
+++ 2.KSQL/300117806/table.sql
@@ -10,10 +10,10 @@ ksql> CREATE TABLE hassana
 >  );
 
 
-CREATE TABLE HASS AS \
-      SELECT message, count(*) \ 
-      FROM systemd \
-      WINDOW TUMBLING (SIZE 30 SECONDS) \
-      WHERE message = 'failed_login' \
-      GROUP BY message \
+CREATE TABLE HASS AS 
+      SELECT message, count(*) 
+      FROM systemd 
+      WINDOW TUMBLING (SIZE 30 SECONDS) 
+      WHERE message = 'failed_login' 
+      GROUP BY message 
       HAVING count(*) > 3;
