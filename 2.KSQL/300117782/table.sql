868fd85f02f3f3f3b6aa9e4e53790efdde2bce35
Raissa111 <56364697+Raissa111@users.noreply.github.com>
2021-04-23 15:16:32 -0400

    Update table.sql

--- 2.KSQL/300117782/table.sql
+++ 2.KSQL/300117782/table.sql
@@ -1,7 +1,7 @@
 
-CREATE TABLE It support AS,
-    SELECT message string, COUNT(*),
-    FROM Erna,
-    WINDOW TUMBLING (SIZE 50 SECONDS),
-    GROUP BY message,
-    EMIT CHANGES; 
+CREATE TABLE It support AS
+SELECT message string, COUNT(*)
+FROM Erna
+WINDOW TUMBLING (SIZE 50 SECONDS)
+GROUP BY message
+EMIT CHANGES; 
